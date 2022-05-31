e7c7831b8a37c381f6b8c2cad7e4c1e02c90a0a8
JakubCzech <jakub.czech.98@gmail.com>
2022-05-31 10:34:09 +0200

    v1.0

---
+++ Czech_Jakub.py
@@ -0,0 +1,35 @@
+import argparse
+import pathlib
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+from processing.utils import perform_processing
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', type=str)
+    parser.add_argument('results_file', type=str)
+    args = parser.parse_args()
+
+    input_file = pathlib.Path(args.input_file)
+    results_file = pathlib.Path(args.results_file)
+
+    data = pd.read_csv(input_file, sep=',')
+
+    output_column_names = ['DECYZJA']
+    input_column_names = ['ZASOB', 'MENAGER_ID', 'GRUPA_1', 'GRUPA_2', 'DZIAL', 'TYTUL', 'OPIS_RODZINY', 'NAZWA_RODZINY', 'KOD_STANOWISKA']
+    gt_data = data.drop(columns=input_column_names)
+    input_data = data.drop(columns=output_column_names)
+
+    predicted_data = perform_processing(input_data)
+    # print(predicted_data.head())
+    # sample for calculating accuracy of the prediction
+    print(accuracy_score(gt_data, predicted_data))
+
+    predicted_data.to_csv(results_file,index=False, sep='\t', encoding='utf-8')
+
+
+if __name__ == '__main__':
+    main()
--- processing/utils.py
+++ processing/utils.py
@@ -11,18 +11,14 @@ def perform_processing(
     # please note, that the predicted data should be a proper pd.DataFrame with column names
     # predicted_data = predict(models, preprocessed_data)
     # return predicted_data
-
     loaded_model = pickle.load(open("processing/model.sav", 'rb'))
     predicted_data = loaded_model.predict(input_data)
-    # column_names = ['ZASOB', 'MENAGER_ID', 'GRUPA_1', 'GRUPA_2', 'DZIAL', 'TYTUL', 'OPIS_RODZINY', 'NAZWA_RODZINY']
-    # predicted_data = predicted_data.drop(columns="DECYZJA", axis=1)
-
+    new = pd.DataFrame(predicted_data)
     # for the simplest approach generate a random DataFrame with proper column names and size
-    # column_names = ['ZASOB', 'MENAGER_ID', 'GRUPA_1', 'GRUPA_2', 'DZIAL', 'TYTUL', 'OPIS_RODZINY', 'NAZWA_RODZINY', 'KOD_STANOWISKA']
+    # column_names = ['DECYZJA']
     # predicted_data = pd.DataFrame(
     #     np.random.randint(low=0, high=2, size=(len(input_data.index), len(column_names))),
     #     columns=column_names
     # )
-    predicted_data = pd.DataFrame(predicted_data, index=False)
-    predicted_data.style.hide_index()
-    return predicted_data
+    print(predicted_data)
+    return new
